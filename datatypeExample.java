bd59fff50dbe4c35f487f09859abd62b8ecb9315
Mirali Jivani <146528110+mirali638@users.noreply.github.com>
2024-08-09 22:01:57 +0530

    Add files via upload

---
+++ datatypeExample.java
@@ -0,0 +1,27 @@
+//this program introduces use of datatypes 
+
+public class datatypeExample {
+    public static void main(String args[]) {
+
+        byte a = 10;
+        short b = 10 * 123;
+        int c = 10000 * 67;
+        long d = 1000000 * 100000 * 128;
+        double e = 99.7896;
+        char f = 'F';
+        boolean g = true;
+        boolean h = false;
+        
+        System.out.println("the value of a = " + a);
+        System.out.println("the value of b = " + b);
+        System.out.println("the value of c = " + c);
+        System.out.println("the value of d = " + d);
+        System.out.println("the value of e = " + e);
+        System.out.println("the value of f = " + f);
+        System.out.println("the value of g = " + g);
+        System.out.println("the value of h = " + h);
+        f++;
+        System.out.println("the value of f after increment = " + f);
+
+    }
+}
