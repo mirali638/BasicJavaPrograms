cef835f99303a76969ad8117436839476dc26594
Mirali Jivani <146528110+mirali638@users.noreply.github.com>
2024-09-04 12:53:13 +0530

    Add files via upload

---
+++ getterssetters.java
@@ -0,0 +1,62 @@
+class Cylinder{
+    private int radius;
+    private int height;
+
+    public Cylinder(int radius, int height) {
+        this.radius = radius;
+        this.height = height;
+    }
+    public int getRadius() {
+        return radius;
+    }
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+    public int getHeight() {
+        return height;
+    }
+    public void setHeight(int height) {
+        this.height = height;
+    }
+    public double surfaceArea(){
+        return 2* Math.PI* radius * radius + 2*Math.PI*radius*height;
+    }
+    public double volume(){
+        return Math.PI * radius * radius * height;
+    }
+}
+
+class Rectangle{
+    private int length;
+    private int breadth;
+
+    public Rectangle() {
+        this.length = 4;
+        this.breadth = 5;
+    }
+    public Rectangle(int length, int breadth) {
+        this.length = length;
+        this.breadth = breadth;
+    }
+    public int getLength() {
+        return length;
+    }
+    public int getBreadth() {
+        return breadth;
+    }
+}
+
+public class getterssetters {
+    public static void main(String[] args) {
+       
+        Cylinder myCylinder = new Cylinder(9, 12);
+        System.out.println(myCylinder.getHeight());
+        System.out.println(myCylinder.getRadius());
+        System.out.println(myCylinder.surfaceArea());
+        System.out.println(myCylinder.volume());
+
+        Rectangle r = new Rectangle(12, 56);
+        System.out.println(r.getLength());
+        System.out.println(r.getBreadth());
+    }
+}
